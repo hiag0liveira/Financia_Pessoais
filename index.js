afdca8b3d1ab2200f76d8f17e9a1a63820227f93
Hiago Oliveira <99771272+hiag0liveira@users.noreply.github.com>
2023-09-03 16:53:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -89,6 +89,7 @@ async function saveTransaction(ev) {
         transactions.splice(indexToRemove, 1, transaction)
         document.querySelector(`#transaction-${id}`).remove()
         renderTransaction(transaction)
+        reload()
     } else {
         const response = await fetch('https://financas-db.vercel.app/transactions', {
             method: 'POST',
@@ -100,11 +101,11 @@ async function saveTransaction(ev) {
         const transaction = await response.json()
         transactions.push(transaction)
         renderTransaction(transaction)
+        reload()
     }
 
     ev.target.reset()
     updateBalance()
-    reload()
 }
 
 async function fetchTransactions() {
